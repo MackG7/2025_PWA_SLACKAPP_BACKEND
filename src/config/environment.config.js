47b1d8dbd2cac2245f7a974acc987657ac9b0c13
MackG7 <mackenson_gustave0409@hotmail.com>
2025-10-07 20:51:31 -0300

    Actualized URL_API_BACKEND

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -5,7 +5,8 @@ dotenv.config()
 const ENVIRONMENT = {
     MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    URL_API_BACKEND:'http://localhost:8080'
 }
 
 export default ENVIRONMENT
